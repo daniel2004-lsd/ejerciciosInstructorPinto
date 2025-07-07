9850c4e48444880ee206a34a65b6f62273565e6b
daniel <danielsalazarvargas953@gmail.com>
2025-07-06 22:11:44 -0500

    boro

--- trabajo_13_junio/Persona.java
+++
@@ -1,8 +0,0 @@
-package trabajosPinto.trabajo_13_junio;
-
-public interface Persona {
-    String getNombre();
-    String getApellido();
-    String getTelefono();
-    String getEstado();
-}
--- trabajo_13_junio/Vehiculo.java
+++
@@ -1,7 +0,0 @@
-package trabajosPinto.trabajo_13_junio;
-
-public interface Vehiculo {
-    void asignarConductor(Conductor conductor);
-    void agregarPersona(Persona persona);
-    void mostrarInfo();
-}
